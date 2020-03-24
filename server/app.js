6eb6dccacdafa9c939a2c5554fd1809d435856c9
Allen <anian100@gmail.com>
2020-03-24 18:44:58 +1300

    set up server

---
+++ server/app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+module.exports = app;
