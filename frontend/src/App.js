ce921fa73ca2f16bdc14d5ce5c83eaf83ae279b9
Allen <anian100@gmail.com>
2020-03-25 19:20:55 +1300

    add material ui and landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-import './App.css';
 import routes from './routes/routes';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#EEEEEE',
+    },
+    secondary: {
+      main: '#111111',
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <div className="App">{routes}</div>
-    </Router>
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <div className="App">{routes}</div>
+      </Router>
+    </MuiThemeProvider>
   );
 }
 
--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
+import styles from '../styles/landingPage.module.css';
 
 function LandingPage() {
   return (
-    <div>
-      <p>This is the landing Page</p>
+    <div className={styles.content}>
+      <Typography variant="h1">SIMUL</Typography>
+      <Link to="/music" style={{ textDecoration: 'none' }}>
+        <Button size="large" color="primary" className={styles.button}>
+          Login to Spotify
+        </Button>
+      </Link>
     </div>
   );
 }
