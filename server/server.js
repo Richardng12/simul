8524976f14c6497fc1b07ade2a540fae316f5586
Edward <ezha731@aucklanduni.ac.nz>
2020-04-01 15:36:52 +1300

    testing

--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
 const app = require('./app');
-const db = require('./src/db');
-const path = require('path');
 
 const port = 8888;
 
-db.connect().then(()=>{
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}!`);
-  });
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
 });
