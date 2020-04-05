9d09e94cdfaf3ce4fb6759939577db409aea443c
Allen <anian100@gmail.com>
2020-04-06 00:02:43 +1200

    refactor backend routes

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,24 +1,10 @@
 const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
-const refresh = require('passport-oauth2-refresh');
-const User = require('../db/models/userModel');
+const access = require('./auth/access');
 
 const router = express.Router();
 
 const spotifyApi = new SpotifyWebApi({});
-
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed. Otherwise, the user will be redirected to the
-//   login page.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-  return null;
-}
 
 /**
  * @swagger
@@ -44,132 +30,26 @@ function ensureAuthenticated(req, res, next) {
  *          content:
  *            application/json:
  */
-router.get('/playlists', ensureAuthenticated, async (req, res) => {
-  let retries = 3;
-
-  const send401Response = () => {
-    return res.status(401).end();
+router.get('/playlists', access.ensureAuthenticated, async (req, res) => {
+  const apiCall = async () => {
+    spotifyApi.setAccessToken(req.user.accessToken);
+    spotifyApi.setRefreshToken(req.user.refreshToken);
+    const result = await spotifyApi.getUserPlaylists();
+    res.status(200).send(result.body);
   };
 
-  User.findById(req.user, (err, user) => {
-    if (err || !user) {
-      return send401Response();
-    }
-
-    const makeRequest = async () => {
-      retries -= 1;
-      if (!retries) {
-        // Couldn't refresh the access token. Tried twice.
-        return send401Response();
-      }
-
-      // Set the credentials and make the request.
-      try {
-        spotifyApi.setAccessToken(user.accessToken);
-        spotifyApi.setRefreshToken(user.refreshToken);
-        const result = await spotifyApi.getUserPlaylists();
-        res.status(200).send(result.body);
-      } catch (error) {
-        if (error.statusCode === 401) {
-          // Access token expired.
-          // Try to fetch a new one.
-          refresh.requestNewAccessToken('spotify', user.refreshToken, (tokenError, accessToken) => {
-            if (tokenError || !accessToken) {
-              return send401Response();
-            }
-
-            // Save the new accessToken for future use
-            // eslint-disable-next-line no-param-reassign
-            user.accessToken = accessToken;
-
-            user.save(() => {
-              makeRequest();
-              // Retry the request.
-            });
-
-            User.updateOne(
-              { _id: req.user.id },
-              {
-                accessToken,
-              },
-            );
-            return null;
-          });
-        } else {
-          // There was another error, handle it appropriately.
-          return send401Response();
-        }
-      }
-      return null;
-    };
-    // Make the initial request.
-    makeRequest();
-    return null;
-  });
+  access.getAccess(apiCall, req, res);
 });
 
-router.get('/userinfo', ensureAuthenticated, async (req, res) => {
-  let retries = 3;
-
-  const send401Response = () => {
-    return res.status(401).end();
+router.get('/userinfo', access.ensureAuthenticated, async (req, res) => {
+  const apiCall = async () => {
+    spotifyApi.setAccessToken(req.user.accessToken);
+    spotifyApi.setRefreshToken(req.user.refreshToken);
+    const result = await spotifyApi.getUserPlaylists();
+    res.status(200).send(result.body);
   };
 
-  User.findById(req.user, (err, user) => {
-    if (err || !user) {
-      return send401Response();
-    }
-
-    const makeRequest = async () => {
-      retries -= 1;
-      if (!retries) {
-        // Couldn't refresh the access token. Tried twice.
-        return send401Response();
-      }
-
-      // Set the credentials and make the request.
-      try {
-        spotifyApi.setAccessToken(req.user.accessToken);
-        spotifyApi.setRefreshToken(req.user.refreshToken);
-        const result = await spotifyApi.getMe();
-        res.status(200).send(result.body);
-      } catch (error) {
-        if (error.statusCode === 401) {
-          // Access token expired.
-          // Try to fetch a new one.
-          refresh.requestNewAccessToken('spotify', user.refreshToken, (tokenError, accessToken) => {
-            if (tokenError || !accessToken) {
-              return send401Response();
-            }
-
-            // Save the new accessToken for future use
-            // eslint-disable-next-line no-param-reassign
-            user.accessToken = accessToken;
-
-            user.save(() => {
-              makeRequest();
-              // Retry the request.
-            });
-
-            User.updateOne(
-              { _id: req.user.id },
-              {
-                accessToken,
-              },
-            );
-            return null;
-          });
-        } else {
-          // There was another error, handle it appropriately.
-          return send401Response();
-        }
-      }
-      return null;
-    };
-    // Make the initial request.
-    makeRequest();
-    return null;
-  });
+  access.getAccess(apiCall, req, res);
 });
 
 /**
@@ -185,132 +65,26 @@ router.get('/userinfo', ensureAuthenticated, async (req, res) => {
  *          content:
  *            application/json:
  */
-router.get('/savedtracks', ensureAuthenticated, async (req, res) => {
-  let retries = 3;
-
-  const send401Response = () => {
-    return res.status(401).end();
+router.get('/savedtracks', access.ensureAuthenticated, async (req, res) => {
+  const apiCall = async () => {
+    spotifyApi.setAccessToken(req.user.accessToken);
+    spotifyApi.setRefreshToken(req.user.refreshToken);
+    const result = await spotifyApi.getMySavedTracks();
+    res.status(200).send(result.body.items);
   };
 
-  User.findById(req.user, (err, user) => {
-    if (err || !user) {
-      return send401Response();
-    }
-
-    const makeRequest = async () => {
-      retries -= 1;
-      if (!retries) {
-        // Couldn't refresh the access token. Tried twice.
-        return send401Response();
-      }
-
-      // Set the credentials and make the request.
-      try {
-        spotifyApi.setAccessToken(req.user.accessToken);
-        spotifyApi.setRefreshToken(req.user.refreshToken);
-        const result = await spotifyApi.getMySavedTracks();
-        res.status(200).send(result.body.items);
-      } catch (error) {
-        if (error.statusCode === 401) {
-          // Access token expired.
-          // Try to fetch a new one.
-          refresh.requestNewAccessToken('spotify', user.refreshToken, (tokenError, accessToken) => {
-            if (tokenError || !accessToken) {
-              return send401Response();
-            }
-
-            // Save the new accessToken for future use
-            // eslint-disable-next-line no-param-reassign
-            user.accessToken = accessToken;
-
-            user.save(() => {
-              makeRequest();
-              // Retry the request.
-            });
-
-            User.updateOne(
-              { _id: req.user.id },
-              {
-                accessToken,
-              },
-            );
-            return null;
-          });
-        } else {
-          // There was another error, handle it appropriately.
-          return send401Response();
-        }
-      }
-      return null;
-    };
-    // Make the initial request.
-    makeRequest();
-    return null;
-  });
+  access.getAccess(apiCall, req, res);
 });
 
-router.post('/createPlaylist', ensureAuthenticated, async (req, res) => {
-  let retries = 3;
-
-  const send401Response = () => {
-    return res.status(401).end();
+router.post('/createPlaylist', access.ensureAuthenticated, async (req, res) => {
+  const apiCall = async () => {
+    spotifyApi.setAccessToken(req.user.accessToken);
+    spotifyApi.setRefreshToken(req.user.refreshToken);
+    const result = await spotifyApi.createPlaylist('My Cool Playlist', { public: false });
+    res.status(200).send(result.body);
   };
 
-  User.findById(req.user, (err, user) => {
-    if (err || !user) {
-      return send401Response();
-    }
-
-    const makeRequest = async () => {
-      retries -= 1;
-      if (!retries) {
-        // Couldn't refresh the access token. Tried twice.
-        return send401Response();
-      }
-
-      // Set the credentials and make the request.
-      try {
-        spotifyApi.setAccessToken(req.user.accessToken);
-        spotifyApi.setRefreshToken(req.user.refreshToken);
-        const result = await spotifyApi.createPlaylist('My Cool Playlist', { public: false });
-        res.status(200).send(result.body);
-      } catch (error) {
-        if (error.statusCode === 401) {
-          // Access token expired.
-          // Try to fetch a new one.
-          refresh.requestNewAccessToken('spotify', user.refreshToken, (tokenError, accessToken) => {
-            if (tokenError || !accessToken) {
-              return send401Response();
-            }
-
-            // Save the new accessToken for future use
-            // eslint-disable-next-line no-param-reassign
-            user.accessToken = accessToken;
-
-            user.save(() => {
-              makeRequest();
-              // Retry the request.
-            });
-
-            User.updateOne(
-              { _id: req.user.id },
-              {
-                accessToken,
-              },
-            );
-            return null;
-          });
-        } else {
-          // There was another error, handle it appropriately.
-          return send401Response();
-        }
-      }
-      return null;
-    };
-    // Make the initial request.
-    makeRequest();
-    return null;
-  });
+  access.getAccess(apiCall, req, res);
 });
 
 module.exports = router;
