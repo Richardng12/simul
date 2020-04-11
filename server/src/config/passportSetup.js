745b956a82ede59903a4236a2dd53506da662bd2
Richard <richardng1998@gmail.com>
2020-04-12 02:17:56 +1200

    added mock strategy for fake auth

--- server/src/config/passportSetup.js
+++ server/src/config/passportSetup.js
@@ -1,35 +1,73 @@
 const passport = require('passport');
-const SpotifyStrategy = require('passport-spotify').Strategy;
+
 const refresh = require('passport-oauth2-refresh');
+const MockStrategy = require('passport-mock-strategy');
+const SpotifyStrategy = require('passport-spotify').Strategy;
 const keys = require('./keys');
 const User = require('../db/models/userModel');
+
+let AppropriateStrategy;
+let options;
+let verifyCallback;
 
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
 //   serialize users into and deserialize users out of the session. Typically,
 //   this will be as simple as storing the user ID when serializing, and finding
 //   the user by ID when deserializing.
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
 
-passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
+if (process.env.NODE_ENV === 'test') {
+  passport.serializeUser((user, done) => {
+    console.log('serializeUser');
     done(null, user);
   });
-});
+
+  passport.deserializeUser((user, done) => {
+    console.log('deserializeUser');
+    done(null, user);
+  });
+} else {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+      done(null, user);
+    });
+  });
+}
 
 // Use the SpotifyStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, expires_in
 //   and spotify profile), and invoke a callback with a user object.
-const strategy = new SpotifyStrategy(
-  {
+
+if (process.env.NODE_ENV === 'test') {
+  AppropriateStrategy = MockStrategy;
+  options = {
+    username: 'Bob12',
+    displayName: 'Bob',
+    spotifyId: '123123123',
+    country: 'NZ',
+    email: 'Apple',
+    thumbnail: 'asdf',
+    profileUrl: 'asdfasdf',
+    accessToken: 'asda',
+    refreshToken: 'zxczxc',
+  };
+  verifyCallback = (user, done) => {
+    return done(null, user);
+  };
+} else {
+  AppropriateStrategy = SpotifyStrategy;
+
+  options = {
     clientID: keys.spotify.clientID,
     clientSecret: keys.spotify.clientSecret,
     callbackURL: '/auth/spotify/callback',
-  },
-  (accessToken, refreshToken, expiresIn, profile, done) => {
+  };
+  verifyCallback = (accessToken, refreshToken, expiresIn, profile, done) => {
     // passport callback function
     // check if user alrdy exists in db
     User.findOne({
@@ -57,16 +95,56 @@ const strategy = new SpotifyStrategy(
           });
       }
     });
+  };
+}
 
-    // asynchronous verification, for effect...
-    process.nextTick(() => {
-      // To keep the example simple, the user's spotify profile is returned to
-      // represent the logged-in user. In a typical application, you would want
-      // to associate the spotify account with a user record in your database,
-      // and return that user instead.
-    });
-  },
-);
+// const strategy = new Strategy(
+//   {
+//     clientID: keys.spotify.clientID,
+//     clientSecret: keys.spotify.clientSecret,
+//     callbackURL: '/auth/spotify/callback',
+//   },
+//   (accessToken, refreshToken, expiresIn, profile, done) => {
+//     // passport callback function
+//     // check if user alrdy exists in db
+//     User.findOne({
+//       spotifyId: profile.id,
+//     }).then(currentUser => {
+//       if (currentUser) {
+//         done(null, currentUser);
+//         // user already in db
+//       } else {
+//         // user not in db
+//         new User({
+//           username: profile.username,
+//           displayName: profile.displayName,
+//           spotifyId: profile.id,
+//           country: profile.country,
+//           email: profile.emails[0].value,
+//           thumbnail: profile.photos[0] == null,
+//           profileUrl: profile.profileUrl,
+//           accessToken,
+//           refreshToken,
+//         })
+//           .save()
+//           .then(newUser => {
+//             done(null, newUser);
+//           });
+//       }
+//     });
 
-passport.use(strategy);
-refresh.use(strategy);
+//     // asynchronous verification, for effect...
+//     process.nextTick(() => {
+//       // To keep the example simple, the user's spotify profile is returned to
+//       // represent the logged-in user. In a typical application, you would want
+//       // to associate the spotify account with a user record in your database,
+//       // and return that user instead.
+//     });
+//   },
+// );
+
+passport.use('spotify', new AppropriateStrategy(options, verifyCallback));
+
+if (process.env.NODE_ENV !== 'test') {
+  refresh.use(new AppropriateStrategy(options, verifyCallback));
+}
--- server/test/app.spec.js
+++ server/test/app.spec.js
@@ -1,5 +1,58 @@
-describe('Sample Test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true);
+/* eslint-disable prefer-template */
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+
+const { before } = require('mocha');
+
+const { expect } = chai;
+const request = require('supertest');
+const app = require('../app');
+
+const authenticatedUser = request.agent(app);
+const unauthenticatedUser = request.agent(app);
+
+describe('GET /account', () => {
+  before(done => {
+    authenticatedUser.get('/auth/spotify').end(() => {
+      done();
+    });
+  });
+
+  it('should return 200 response when user is logged in', done => {
+    authenticatedUser.get('/account').end((req, res) => {
+      expect(res.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+  it('should return 302 response when user is logged in', done => {
+    unauthenticatedUser.get('/account').end((req, res) => {
+      expect(res.statusCode).to.be.equal(302);
+      done();
+    });
+  });
+  it('should return 200 response when authenticated user goes on landing page', done => {
+    authenticatedUser.get('/').end((req, res) => {
+      expect(res.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+  it('should return 200 response when unauthenticated user goes on landing page', done => {
+    unauthenticatedUser.get('/').end((req, res) => {
+      expect(res.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+  it('should return 200 response when authenticated user accesses lobbies', done => {
+    authenticatedUser.get('/lobbies').end((req, res) => {
+      expect(res.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+  it('should return 302 response when unauthenticated user accesses lobbies', done => {
+    unauthenticatedUser.get('/lobbies').end((req, res) => {
+      expect(res.statusCode).to.be.equal(302);
+      done();
+    });
   });
 });
