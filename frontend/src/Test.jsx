72f853ecafb9bf319dd46b7e83203ca605f53c89
Sabatr <bnguyen12152@gmail.com>
2020-04-12 11:50:16 +1200

    Added player with react-spotify-web-playback library

--- frontend/src/Test.jsx
+++ frontend/src/Test.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import SpotifyWebPlayer from 'react-spotify-web-playback';
 import { getUserInfo } from './store/profile/profileActions';
 
 const Test = props => {
-  const { userInfo } = props;
+  const { userInfo, accessToken } = props;
+
+  const [showPlayer, setShowPlayer] = useState(false);
+  useEffect(() => {
+    userInfo();
+  }, []);
+
   return (
-    <button onClick={() => userInfo()} type="button">
-      Hello
-    </button>
+    <div>
+      <button onClick={() => setShowPlayer(true)} type="button">
+        Hello
+      </button>
+      {showPlayer ? (
+        <SpotifyWebPlayer
+          token={accessToken}
+          uris={[
+            'spotify:track:6Ozh9Ok6h4Oi1wUSLtBseN',
+            'spotify:track:6HbI4e2Y2f6HYVV6r04M4W',
+            'spotify:track:7m9OqQk4RVRkw9JJdeAw96',
+          ]}
+          autoPlay
+        />
+      ) : null}
+    </div>
   );
 };
 
-// const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  accessToken: state.profileReducer.token,
+});
 
 const mapDispatchToProps = dispatch => ({
   userInfo: bindActionCreators(getUserInfo, dispatch),
 });
-export default connect(null, mapDispatchToProps)(Test);
+export default connect(mapStateToProps, mapDispatchToProps)(Test);
