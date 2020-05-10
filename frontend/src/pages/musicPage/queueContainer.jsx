3f7e8318bd045a123f54366d3f3e615d6c7e7a77
Sabatr <bnguyen12152@gmail.com>
2020-05-10 16:09:26 +1200

    Fixed issue where player didn't load on start

--- frontend/src/pages/musicPage/queueContainer.jsx
+++ frontend/src/pages/musicPage/queueContainer.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import SongSearch from './components/songSearch';
 import SongQueueTable from './components/songQueueTable';
-import MusicPlayer from './components/musicPlayer';
 import styles from './styles/queueContainer.module.css';
+import MusicPlayerContainer from './components/musicPlayerContainer';
 
 const QueueContainer = () => {
   return (
     <div className={styles.root}>
       <SongSearch />
       <SongQueueTable />
-      <MusicPlayer />
+      <MusicPlayerContainer />
     </div>
   );
 };
