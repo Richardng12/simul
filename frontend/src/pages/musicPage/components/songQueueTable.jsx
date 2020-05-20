bc18275c16ae902353d2fc1957c87fd655ab9668
Sabatr <bnguyen12152@gmail.com>
2020-05-21 01:12:47 +1200

    Styled queue of songs

--- frontend/src/pages/musicPage/components/songQueueTable.jsx
+++ frontend/src/pages/musicPage/components/songQueueTable.jsx
@@ -1,13 +1,8 @@
+/* eslint no-unused-vars: 0 */
 import React from 'react';
 import { bindActionCreators } from 'redux';
-import Table from '@material-ui/core/Table';
+import classNames from 'classnames';
 import ClearIcon from '@material-ui/icons/Clear';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import Paper from '@material-ui/core/Paper';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import { connect } from 'react-redux';
 import { removeSongFromQueue } from '../../../store/lobby/lobbyActions';
 import styles from '../styles/songQueueTable.module.css';
@@ -23,36 +18,32 @@ const SongQueueTable = props => {
   );
 
   return (
-    <TableContainer component={Paper} className={styles.table}>
-      <Table stickyHeader aria-label="simple table">
-        <TableHead className={styles.tableHead}>
-          <TableRow>
-            <TableCell>TITLE</TableCell>
-            <TableCell align="left">ARTIST</TableCell>
-            <TableCell align="right" />
-          </TableRow>
-        </TableHead>
-        <TableBody className={styles.tableBody}>
-          {rows.map(row => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.title}
-              </TableCell>
-              <TableCell align="left">{row.artist}</TableCell>
-              <TableCell align="right">
-                {row.addedBy === userId && (
-                  <ClearIcon
-                    onClick={() => {
-                      removeSong(row.id);
-                    }}
-                  />
-                )}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div className={styles.table}>
+      <div className={classNames(styles.row, styles.topRow)}>
+        <p className={styles.titleHeader}>TITLE</p>
+        <p className={styles.artistHeader}>ARTIST</p>
+      </div>
+      {rows.map((row, index) => (
+        <div className={styles.row}>
+          <p className={classNames(styles.rowTitle, index === 0 ? styles.highlight : '')}>
+            {row.title}
+          </p>
+          <p className={classNames(styles.rowArtist, index === 0 ? styles.highlight : '')}>
+            {row.artist}
+          </p>
+          <div>
+            {row.addedBy === userId && (
+              <ClearIcon
+                className={styles.clearIcon}
+                onClick={() => {
+                  removeSong(row.id);
+                }}
+              />
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
