9114de280e620ff37f05a364abf960ad75685c36
Allen <anian100@gmail.com>
2020-05-23 21:24:43 +1200

    add snapshot tests for music page

--- frontend/src/test/MusicPage.test.js
+++ frontend/src/test/MusicPage.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import configureMockStore from 'redux-mock-store';
 import MusicPage from '../pages/musicPage/musicPage';
 import AppBar from '../pages/musicPage/components/appBar';
+import LyricsContainer from '../pages/musicPage/lyricsContainer';
+import SongSearch from '../pages/musicPage/components/songSearch';
+import SongQueueTable from '../pages/musicPage/components/songQueueTable';
+import MembersList from '../pages/musicPage/components/membersList';
 
 const mockStore = configureMockStore();
 
@@ -19,15 +22,29 @@ describe('<MusicPage />', () => {
         isAuthenticated: false,
         message: '',
       },
+      musicReducer: {
+        currentSong: null,
+        currentDevice: null,
+      },
+      lobbyReducer: {
+        lobbies: [],
+        loading: false,
+        currentLobby: null,
+        lobbyId: '',
+        users: [],
+        currentQueue: [],
+      },
     });
   });
   it('renders without crashing', () => {
-    const component = shallow(
-      <Provider store={store}>
-        <MusicPage />
-      </Provider>,
+    const component = renderer.create(
+      <MemoryRouter>
+        <Provider store={store}>
+          <MusicPage />
+        </Provider>
+      </MemoryRouter>,
     );
-    expect(component).toBeTruthy();
+    expect(component).toMatchSnapshot();
   });
 
   it('renders app bar', () => {
@@ -37,6 +54,42 @@ describe('<MusicPage />', () => {
           <AppBar />
         </Provider>
       </MemoryRouter>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders lyrics container', () => {
+    const tree = renderer.create(
+      <Provider store={store}>
+        <LyricsContainer />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders song search', () => {
+    const tree = renderer.create(
+      <Provider store={store}>
+        <SongSearch />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders song queue table', () => {
+    const tree = renderer.create(
+      <Provider store={store}>
+        <SongQueueTable />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders members list', () => {
+    const tree = renderer.create(
+      <Provider store={store}>
+        <MembersList />
+      </Provider>,
     );
     expect(tree).toMatchSnapshot();
   });
